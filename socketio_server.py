9b1fcd1461169959607a6414367d5656df49c7cb
Bemoi Erian <bemoi.erian@gmail.com>
2024-02-09 04:30:55 +0200

    added decoding

--- socketio_server.py
+++ socketio_server.py
@@ -1,9 +1,14 @@
+import time
 import socketio
 import eventlet
 import numpy as np
 import cv2
+import base64
 
 sio = socketio.Server()
+# Calculate frame rate
+# frame_count = 0
+# start_time = time.time()
 
 
 @sio.on("connect")
@@ -13,12 +18,13 @@ def connect(sid, environ):
 
 @sio.on("frame")
 def frame(sid, data):
-    # print(f"Frame received, {data}")
-    # Convert byte array back to color array
-    # print(data)
+    # global frame_count, start_time
+
     height = data["height"]
     width = data["width"]
     frame = data["frame"]
+    # Decode the Base64 string back to a byte array
+    frame = base64.b64decode(frame)
     color_array = np.frombuffer(frame, dtype=np.uint8)
 
     # Reshape the color array back to an image
@@ -31,11 +37,25 @@ def frame(sid, data):
     # Flip the image vertically
     img = cv2.flip(img, 0)
 
-    # Save the image locally
-    cv2.imwrite("frame.png", img)
+    # Save the image locally, name it with timestamp
+    # cv2.imwrite(f"./frames_test/frame_{time.time()}.png", img)
+
+    print("Frame processed successfully!")
+
+    # frame_count += 1
+    # elapsed_time = time.time() - start_time
+
+    # if elapsed_time > 0:  # Avoid division by zero
+    #     frame_rate = frame_count / elapsed_time
+    #     print(f"Frame rate: {frame_rate} frames per second")
+
+    # # Reset the frame count every second to get a running frame rate
+    # if elapsed_time > 1.0:
+    #     frame_count = 0
+    #     start_time = time.time()
 
     # sio.emit("result", [])
-    sio.emit("result", "Frame Processed Successfully!")
+    sio.emit("result", f"{time.time()} Frame Processed Successfully!")
 
 
 @sio.on("disconnect")
