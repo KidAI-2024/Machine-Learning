342d15849f4b3443726d694493b0772f79f25692
Bemoi Erian <bemoi.erian@gmail.com>
2024-03-04 20:23:51 +0200

    use handpose utils in handpose classifier for testing

--- HandPoseClassifier/hand_pose_classifier.py
+++ HandPoseClassifier/hand_pose_classifier.py
@@ -1,9 +1,13 @@
+from .hand_pose_utils import HandPoseUtils
+
+
 class HandPoseClassifier:
     def __init__(self):
-        pass
+        self.hand_pose_utils = HandPoseUtils()
 
     def preprocess(self, data):
-        pass
+        land_marks = self.hand_pose_utils.get_hand_landmarks(data)
+        return land_marks
 
     def train(self, data):
         pass
