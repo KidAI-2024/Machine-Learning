e18729af8e3d517517fac64934124cc36ab0de0c
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2024-07-02 02:59:59 +0300

    classical ML models init

--- ImageClassifier/image_classifier_utils.py
+++ ImageClassifier/image_classifier_utils.py
@@ -405,3 +405,43 @@ class Cifar10CnnModel(ImageClassificationBaseCNN):
 
     def forward(self, xb):
         return self.network(xb)
+
+
+class LogisticRegressionModel(nn.Module):
+
+    def __init__(self, input_size, num_classes):
+        super().__init__()
+        self.input_size = input_size
+        self.num_classes = num_classes
+        self.linear = nn.Linear(input_size, num_classes)
+
+    def forward(self, xb):
+        out = self.linear(xb)
+        return out
+
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)  # Calculate accuracy
+        return {"val_loss": loss, "val_acc": acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x["val_loss"] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x["val_acc"] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        return {"val_loss": epoch_loss.item(), "val_acc": epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print(
+            "Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(
+                epoch, result["val_loss"], result["val_acc"]
+            )
+        )
