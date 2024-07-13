2cc77f57f45b55d013f7a73fd43d0d5692b217e9
Bemoi Erian <bemoi.erian@gmail.com>
2024-07-13 13:54:03 +0300

    added more hand pose tests

--- Tests/test_hand_pose_handlers.py
+++ Tests/test_hand_pose_handlers.py
@@ -5,6 +5,147 @@ from server_utils import Req, Res
 
 
 class TestHandPoseHandlers:
+    def test_train_success(self, mocker):
+        """
+        tests the train_hand_pose function in case of success
+        should return a status of success
+        """
+        mocker.patch.object(HandPoseClassifier, "preprocess")
+        mocker.patch.object(HandPoseClassifier, "train")
+        mocker.patch.object(
+            HandPoseClassifier, "feature_importance_graph"
+        ).return_value = "image_string"
+        mocker.patch("handlers.hand_pose_handlers.utils.read_data").return_value = {
+            "0": ["image1.png", "image2.png"],
+            "1": ["image3.png", "image4.png"],
+        }
+        mocker.patch.object(HandPoseClassifier, "save")
+        path = "path_to_model"
+        model = "SVM"
+        feature_extraction_type = "mediapipe"
+        saved_model_name = "hand_pose_model.pkl"
+        feature_importance_graph = "image_string"
+        msg = {
+            "path": path,
+            "model": model,
+            "feature_extraction_type": feature_extraction_type,
+            "features": "feature1,feature2",
+        }
+        event = "train_hand_pose"
+        req = Req()
+        req.msg = msg
+        req.event = event
+        res = Res()
+        result = train_hand_pose(req, res)
+
+        # Assert that the result is as expected
+        assert result == {
+            "event": event,
+            "status": "success",
+            "saved_model_name": saved_model_name,
+            "feature_importance_graph": feature_importance_graph,
+            "training_accuracy": None,
+        }
+
+    def test_train_no_features(self, mocker):
+        """
+        tests the train_hand_pose function in case of failure (the user has not selected any features)
+        should return a status of failed
+        """
+        path = "path_to_models"
+        model = "SVM"
+        feature_extraction_type = "mediapipe"
+        msg = {
+            "path": path,
+            "model": model,
+            "feature_extraction_type": feature_extraction_type,
+            "features": "",
+        }
+        event = "train_hand_pose"
+        req = Req()
+        req.msg = msg
+        req.event = event
+        res = Res()
+        result = train_hand_pose(req, res)
+
+        # Assert that the result is as expected
+        assert result == {
+            "event": event,
+            "status": "failed",
+            "error": "Select some features to train the model",
+        }
+
+    def test_train_invalid_images(self, mocker):
+        """
+        tests the train_hand_pose function in case of failure (preprocessing exception)
+        should return a status of failed
+        """
+        mocker.patch.object(HandPoseClassifier, "train")
+        mocker.patch.object(HandPoseClassifier, "preprocess").side_effect = Exception
+        path = "path_to_model"
+        model = "SVM"
+        feature_extraction_type = "mediapipe"
+        msg = {
+            "path": path,
+            "model": model,
+            "feature_extraction_type": feature_extraction_type,
+            "features": "feature1,feature2",
+        }
+        event = "train_hand_pose"
+        req = Req()
+        req.msg = msg
+        req.event = event
+        res = Res()
+        result = train_hand_pose(req, res)
+
+        # Assert that the result is as expected
+        assert result == {
+            "event": event,
+            "status": "failed",
+            "error": "Training data contains invalid images",
+        }
+
+    def test_train_feature_importace_graph_error(self, mocker):
+        """
+        tests the train_hand_pose function in case of failure (feature_importance_graph exception)
+        should return a status of success
+        """
+        mocker.patch.object(HandPoseClassifier, "train")
+        mocker.patch.object(HandPoseClassifier, "preprocess")
+        mocker.patch.object(
+            HandPoseClassifier, "feature_importance_graph"
+        ).side_effect = Exception
+        mocker.patch("handlers.hand_pose_handlers.utils.read_data").return_value = {
+            "0": ["image1.png", "image2.png"],
+            "1": ["image3.png", "image4.png"],
+        }
+        mocker.patch.object(HandPoseClassifier, "save")
+        path = "path_to_model"
+        model = "SVM"
+        feature_extraction_type = "mediapipe"
+        saved_model_name = "hand_pose_model.pkl"
+        msg = {
+            "path": path,
+            "model": model,
+            "feature_extraction_type": feature_extraction_type,
+            "features": "feature1,feature2",
+        }
+        event = "train_hand_pose"
+        req = Req()
+        req.msg = msg
+        req.event = event
+        res = Res()
+        result = train_hand_pose(req, res)
+
+        # Assert that the result is as expected
+        assert result == {
+            "event": event,
+            "status": "success",
+            "saved_model_name": saved_model_name,
+            "feature_importance_graph": "",
+            "training_accuracy": None,
+        }
+
     def test_predict_without_training(self, black_image_string):
         """
         tests the predict function without training the model
